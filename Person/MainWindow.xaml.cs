17777fafec71b6aa7f5accda8eade4425ffaffd1
JinaMartin <mata.jina@seznam.cz>
2021-05-09 23:14:01 +0200

    Přidejte soubory projektu.

---
+++ Person/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Person
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PersonClass.InitPeople();
+            PersonClass p = new PersonClass();
+            People.DataContext = p;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            People.SelectedIndex = 0; 
+        }
+
+        private void Person_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            PersonClass pc = (PersonClass)((sender as ListBox).SelectedItem);
+            DataContext = pc;
+        }
+
+        private void People_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            PersonClass pc = (PersonClass)((sender as ListBox).SelectedItem);
+           PersonEdit pce = new PersonEdit(pc);
+            pce.ShowDialog();
+        }
+    }
+}
---
+++ Person/PersonClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+
+namespace Person
+{
+    public class PersonClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set { name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name")); }
+        }
+        private string surname;
+        public string Surname
+        {
+            get => surname;
+            set { surname = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Surname")); }
+        }
+
+
+
+        private DateTime date;
+        public DateTime Date
+        {
+            get => date;
+            set { date = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Date")); }
+        }
+
+
+
+        public static ObservableCollection<PersonClass> AllPeople { get; set; } = new ObservableCollection<PersonClass>();
+
+        public static void InitPeople()
+        {
+            AllPeople.Add( new PersonClass
+            {
+                Name = "Martin",
+                Surname = "Jína",
+                Date = new DateTime(2003, 8, 22, 6, 15, 0),
+              
+            });
+            AllPeople.Add(new PersonClass
+            {
+                Name = "Martin",
+                Surname = "Král",
+                Date = new DateTime(2002, 5, 7, 3, 30, 0),
+               
+            });
+            AllPeople.Add(new PersonClass
+            {
+                Name = "Klára",
+                Surname = "Nová",
+                Date = new DateTime(1995, 8, 16, 14, 10, 0),
+               
+            });
+            AllPeople.Add(new PersonClass
+            {
+                Name = "Ronald",
+                Surname = "McDonald",
+                Date = new DateTime(1963, 1, 5, 12, 0, 0),
+               
+            });
+        }
+    }
+}
